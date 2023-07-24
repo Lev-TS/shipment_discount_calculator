43baaf28b2b4e06d98d3f6cdf555a75d716d4117
Levan Tsutskiridze <levan@maknatuna.local>
2023-07-24 10:05:50 +0300

    adds template

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+    },
+  },
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+};
